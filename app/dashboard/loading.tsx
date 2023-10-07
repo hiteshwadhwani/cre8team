31ea7d686ac5558006fb532a5f555b67fd3c6e44
hitesh wadhwani <developerhitesh1403@gmail.com>
2023-10-07 15:30:22 +0530

    updated

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,52 +1,16 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 
 export default function Loading() {
+  const loadingDiv = Array.from({ length: 6 }, (_, idx) => idx);
   return (
     <div>
-    <div className="flex justify-center mt-2">
-      <div className="w-32 h-14 bg-gray-100 rounded-md m-2 p-3 shadow-md animate-pulse ">
-        <div className="w-full h-8 bg-gray-200 mb-4 rounded"></div>
-        
+      <Skeleton className="mt-6 h-10 w-20" />
+      <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-[40px] gap-y-[40px]">
+        {loadingDiv.map((item, idx) => (
+          <Skeleton key={idx} className="h-[320px] rounded-[20px]" />
+        ))}
       </div>
     </div>
-
-    <div className=" flex flex-wrap p-5 justify-evenly ">
-      <div className="w-[400px] h-72 bg-gray-100 rounded-md m-2 md:mx-10 md:mb-10 md:p-6 p-3 shadow-md animate-pulse ">
-        <div className="w-full h-8 bg-gray-200 mb-4 rounded"></div>
-        <div className="w-full h-24 bg-gray-200 mb-4 rounded"></div>
-        <div className="w-3/4 h-8 bg-gray-200 mb-4 rounded"></div>
-      </div>
-
-      <div className="w-[400px] h-72 bg-gray-100 rounded-md m-2 md:mx-10 md:mb-10 md:p-6 p-3 shadow-md animate-pulse ">
-        <div className="w-full h-8 bg-gray-200 mb-4 rounded"></div>
-        <div className="w-full h-24 bg-gray-200 mb-4 rounded"></div>
-        <div className="w-3/4 h-8 bg-gray-200 mb-4 rounded"></div>
-      </div>
-
-      <div className="w-[400px] h-72 bg-gray-100 rounded-md m-2 md:mx-10 md:mb-10 md:p-6 p-3 shadow-md animate-pulse ">
-        <div className="w-full h-8 bg-gray-200 mb-4 rounded"></div>
-        <div className="w-full h-24 bg-gray-200 mb-4 rounded"></div>
-        <div className="w-3/4 h-8 bg-gray-200 mb-4 rounded"></div>
-      </div>
-
-      <div className="w-[400px] h-72 bg-gray-100 rounded-md m-2 md:mx-10 md:mb-10 md:p-6 p-3 shadow-md animate-pulse ">
-        <div className="w-full h-8 bg-gray-200 mb-4 rounded"></div>
-        <div className="w-full h-24 bg-gray-200 mb-4 rounded"></div>
-        <div className="w-3/4 h-8 bg-gray-200 mb-4 rounded"></div>
-      </div>
-
-      <div className="w-[400px] h-72 bg-gray-100 rounded-md m-2 md:mx-10 md:mb-10 md:p-6 p-3 shadow-md animate-pulse ">
-        <div className="w-full h-8 bg-gray-200 mb-4 rounded"></div>
-        <div className="w-full h-24 bg-gray-200 mb-4 rounded"></div>
-        <div className="w-3/4 h-8 bg-gray-200 mb-4 rounded"></div>
-      </div>
-
-      <div className="w-[400px] h-72 bg-gray-100 rounded-md m-2 md:mx-10 md:mb-10 md:p-6 p-3 shadow-md animate-pulse ">
-        <div className="w-full h-8 bg-gray-200 mb-4 rounded"></div>
-        <div className="w-full h-24 bg-gray-200 mb-4 rounded"></div>
-        <div className="w-3/4 h-8 bg-gray-200 mb-4 rounded"></div>
-      </div>
-    </div>
-  </div>
   );
 }
--- components/modal/profile-modal.tsx
+++ components/modal/profile-modal.tsx
@@ -3,22 +3,171 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import Modal from "../ui/modal";
 import useModal from "@/hooks/use-modal";
-import * as z from "zod"
- 
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useUser } from "@/hooks/use-user";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
+import { Rocket } from "lucide-react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+const MAX_SIZE_MB = 1;
+
 const formSchema = z.object({
-  email: z.string(),
-  name: z.string(),
-  imageUrl: z.string()
-})
+  name: z.string().min(1),
+  imageUrl: z.custom<FileList>((val) => val instanceof FileList, "Required")
+  .transform((file) => file.length > 0 && file.item(0))
+  .refine(
+    (file) => !file || (!!file && file.size <= MAX_SIZE_MB * 1024 * 1024),
+    {
+      message: `The profile picture must be a maximum of ${MAX_SIZE_MB}MB.`,
+    }
+  ),
+  profession: z.string().min(1),
+  about: z.string().min(1),
+  resume: z
+    .custom<FileList>((val) => val instanceof FileList, "Required")
+    .transform((file) => file.length > 0 && file.item(0))
+    .refine(
+      (file) => !file || (!!file && file.size <= MAX_SIZE_MB * 1024 * 1024),
+      {
+        message: `The profile picture must be a maximum of ${MAX_SIZE_MB}MB.`,
+      }
+    )
+    .refine(
+      (file) => !file || (!!file && file.type?.startsWith("application/pdf")),
+      {
+        message: "Only PDFs are allowed to be sent.",
+      }
+    ),
+});
 
 const ProfilePage = () => {
-    const {isOpen, type, setClose} = useModal()
+  const supabase = createClientComponentClient();
+  const { isOpen, type, setClose } = useModal();
+  const [file, setFile] = useState<FileList | null>();
+  const userData = useUser();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: userData?.profile?.name ?? undefined,
+      profession: userData?.profile?.profession ?? undefined,
+      about: userData?.profile?.about ?? undefined,
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    //steps
+    //setup cloudinary
+    //save files to the cloudinary
+    //save data to db
+    const bucket = "resume";
+    console.log(values);
+  };
 
   return (
-    <Modal isOpen={isOpen && type === 'profile-modal'} onClose={setClose}>
-        <div>
-            
-        </div>
+    <Modal
+      title="Profile"
+      isOpen={isOpen && type === "profile-modal"}
+      onClose={setClose}
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Full Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your full name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="profession"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Profession</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your profession" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="about"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>About Me</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Tell us a little about yourself..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field: {onChange}, ...field }) => (
+              <FormItem>
+                <FormLabel>Change your profile photo</FormLabel>
+                <FormControl>
+                  <Input
+                    className="cursor-pointer"
+                    accept=".jpg,.png,.jpeg"
+                    type="file"
+                    {...field}
+                    onChange={(e) => onChange(e.target.files)}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="resume"
+            render={({ field: { onChange }, ...field }) => (
+              <FormItem>
+                <FormLabel>Upload your resume</FormLabel>
+                <FormControl>
+                  <Input
+                    className="cursor-pointer"
+                    accept=".pdf"
+                    type="file"
+                    {...field}
+                    onChange={(e) => onChange(e.target.files)}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">
+            Lets Go <Rocket className="ml-1 h-4 w-4" />
+          </Button>
+        </form>
+      </Form>
     </Modal>
   );
 };
